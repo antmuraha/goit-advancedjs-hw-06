7767493a98d710d18fbf0857a0d9f3e0f8482ec9
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2025-06-15 22:29:14 +0300

    generic task 7: using Record

--- src/generic/7.ts
+++ src/generic/7.ts
@@ -1,6 +1,6 @@
 /*
-  У вас є перелік UserRole, який використовується для класифікації користувачів у вашому додатку.
-  Ви хочете створити об'єкт RoleDescription, який зіставлятиме кожну роль користувача з її описом.
+  You have a UserRole enum that is used to classify users in your application.
+  You want to create a RoleDescription object that maps each user role to its description.
 */
 
 export enum UserRole {
@@ -9,11 +9,11 @@ export enum UserRole {
   guest = 'guest',
 }
 
-// Замініть наступний код на версію за допомогою Record
-const RoleDescription = {
-  admin: 'Admin User',
-  editor: 'Editor User',
-  guest: 'Guest User',
+// Replace the following code with a version using Record
+const RoleDescription: Record<UserRole, string> = {
+  [UserRole.admin]: 'Admin User',
+  [UserRole.editor]: 'Editor User',
+  [UserRole.guest]: 'Guest User',
 };
 
 export {};
