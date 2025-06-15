ea80d4b84a9c01f5e204035e9e01f292e3e5bd7c
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2025-06-15 21:58:59 +0300

    task 5: union & literal types

--- src/baseTypes/5.ts
+++ src/baseTypes/5.ts
@@ -1,7 +1,7 @@
 /* 
-  Як ви визначите змінну в TypeScript, яка може приймати рядок або число (union type)? 
-  І так само визначте змінну, яка може приймати тільки одне з двох рядкових значень: 'enable' або 'disable' (literal type)?
+  How do you declare a variable in TypeScript that can accept either a string or a number (union type)?
+  And also declare a variable that can accept only one of two string values: 'enable' or 'disable' (literal type)?
 */
 
-let union;
-let literal;
+let union: string | number;
+let literal: "enable" | "disable";
