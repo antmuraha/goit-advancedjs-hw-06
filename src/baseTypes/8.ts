296f80fe6fd66331c489b3111c5ff8166b44a3ca
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2025-06-15 22:07:32 +0300

    task 8: union type

--- src/baseTypes/8.ts
+++ src/baseTypes/8.ts
@@ -1,8 +1,10 @@
 /*
-  Створіть тип "Gender", використовуючи union type, 
-  який може містити значення "male", "female". Створіть змінну myGender цього типу.
+  Create a type "Gender" using a union type,
+  which can have the values "male" or "female". Create a variable myGender of this type.
 */
 
-const myGender;
+type Gender = "male" | "female";
+
+const myGender: Gender;
 
 export {};
