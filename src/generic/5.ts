7a133be5e0a1ad7bccc20f3da341f01438202a9a
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2025-06-15 22:24:53 +0300

    generic task 5: describes a key-value pair

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -1,11 +1,11 @@
 /*
-  Вам потрібно реалізувати інтерфейс KeyValuePair, який описує пару ключ-значення. 
-  Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
+  You need to implement the KeyValuePair interface, which describes a key-value pair.
+  Use generics so that this interface can work with any types of keys and values.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<K, V> {
+  key: K;
+  value: V;
 }
 
 export {};
