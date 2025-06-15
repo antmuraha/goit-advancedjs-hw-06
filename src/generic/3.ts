37d2c5c809fe99223e7b0b5ee1ac4f4c4b125795
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2025-06-15 22:21:06 +0300

    generic task 3: merge function that combines two objects

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -1,10 +1,10 @@
 /*
-  У вас є функція merge, яка поєднує два об'єкти. 
-  Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
+  You have a merge function that combines two objects.
+  Use generics to specify that these objects can be of any type.
 */
 
-function merge (objA, objB) {
-  return Object.assign(objA, objB);
+function merge<T, U>(objA: T, objB: U): T & U {
+  return Object.assign({}, objA, objB);
 }
 
 export {}
